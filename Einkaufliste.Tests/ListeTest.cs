cd97b518ac1a3f0f4cdff887c1ae839fbb639ecf
M. Thaler <vs.schema.form@gmail.com>
2020-01-28 17:01:33 +0100

    Add Test Project

---
+++ Einkaufliste.Tests/ListeTest.cs
@@ -0,0 +1,34 @@
+using Einkaufsliste;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Einkaufliste.Tests
+{
+    public class ListeTest
+    {
+        public class ListeTest1
+        {
+            public EinkaufServiceTest einkaufServiceTest { get; set; }
+
+            public ListeTest1()
+            {
+                Init();
+            }
+
+            internal async void Init()
+            {
+                einkaufServiceTest = new EinkaufServiceTest();
+                einkaufServiceTest.List = await einkaufServiceTest.GetList();
+            }
+
+            [Fact]
+            public void AddItem()
+            {
+                Assert.Equal(3, einkaufServiceTest.List.Count());
+                einkaufServiceTest.AddEinkauf(new Einkauf() { Name = "Banane" });
+                Assert.Equal(4, einkaufServiceTest.List.Count());
+            }
+        }
+    }
+}
