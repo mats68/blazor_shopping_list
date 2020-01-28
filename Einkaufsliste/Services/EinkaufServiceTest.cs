9c456644a92e3d0595d0465e8a6061b07fe617ee
M. Thaler <vs.schema.form@gmail.com>
2020-01-28 10:09:59 +0100

    Add project files.

---
+++ Einkaufsliste/Services/EinkaufServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Einkaufsliste
+{
+    public class EinkaufServiceTest : IEinkaufService
+    {
+        public List<Einkauf> GetList()
+        {
+            return new List<Einkauf>()
+            {
+                new Einkauf(){Name = "Radieschen"},
+                new Einkauf(){Name = "Brot"},
+                new Einkauf(){Name = "Käse"},
+            };
+
+        }
+    }
+}
---
+++ Einkaufsliste/Startup.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Einkaufsliste
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<EinkaufServiceTest>();
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
