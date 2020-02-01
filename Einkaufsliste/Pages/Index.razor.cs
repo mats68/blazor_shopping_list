4e7baa9f7533cf06dbdf21a36f0cd34561c345fb
M. Thaler <vs.schema.form@gmail.com>
2020-02-01 12:43:53 +0100

    refactor code behind for page

---
+++ Einkaufsliste/Pages/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿using Einkaufsliste.Components.ListExt;
+using Einkaufsliste.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Einkaufsliste.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        [Inject]
+        public ListServices ListServices { get; set; }
+        public ListService ListEinkauf
+        {
+            get
+            {
+                return ListServices.ListEinkauf;
+            }
+        }
+
+        public void ShowFavoriten()
+        {
+            Console.WriteLine("ShowFavoriten");
+        }
+    }
+}
